182f5dcb04a349ab4ab549fb9d63b2448fde47d2
Hikari-no-Mika <139961497+Hikari-no-Mika@users.noreply.github.com>
2024-01-19 04:42:59 +0300

    done 03mod\ -28, -30

--- Course_1/js/03mod-functions/28-closure.js
+++ Course_1/js/03mod-functions/28-closure.js
@@ -6,28 +6,43 @@
 
 
 /* Напишите функцию sumN, которая возвращает другую функцию. Возвращаемая должна складывать произвольное число с начальным N  */
+{
+    function sumN(n) {
+        function s(m) {
+            return n + m;
+        }
 
-function sumN(n) {
-  function s(m) {
-    return n + m;
-  }
-  return s;
-}
-const foo = sumN(10);
+        return s;
+    }
+
+    const foo = sumN(10);
 
 // console.log(typeof foo);
 // console.log(foo);
 
-// console.log(foo(5)); //15
-// console.log(sumN(10)(5)); //15
-
+    console.log(foo(5)); //15
+    console.log(sumN(10)(5)); //15
+}
 
 // Напишите функцию start(x,y), которая принимает произвольные
 // целочисленные начальные координаты x и y, а возвращает функцию
 // move(dx, dy), которая меняет исходные координаты на величины dx и dy
 // При вызове fn должны выводиться новые координаты, с учетом смещения на  dx и dy
 // Например,
-// const move = start(10, 10);
-// move(2, 3); //12, 13
-// move(-7, -13); //5, 0
-// move(5, 23); //10, 23
+{
+    function start(x, y) {
+        return function move(dx, dy) {
+            x += dx;
+            y += dy;
+            return `x=${x}, y=${y}`;
+        }
+        return move;
+    }
+
+    const move = start(10, 10);
+    console.log(move(2, 3)); //12, 13
+    console.log(move(-7, -13)); //5, 0
+    console.log(move(5, 23)); //10, 23
+    const move2 = start(5, 7); //даже с дописанной 2 функция move находится и воспроизводится
+    console.log(move2(5, 4)); //10, 11
+}
--- Course_1/js/03mod-functions/30-arguments.js
+++ Course_1/js/03mod-functions/30-arguments.js
@@ -4,7 +4,7 @@
 - нахождение суммы набора чисел
 - склейка набора строк
 - нахождение арифметического среднего
-- установка/полученеи данных для диаграммы
+- установка/получение данных для диаграммы
 */
 
 /*
@@ -28,28 +28,40 @@ length - длина
 // //               0  1  2  3
 // console.log(sum(10, 5, 9, 2)); //26
 
-// Пример кода с rest-оператором 
-function sum( ...a ) {
-  let s = 0;
-  let len = a.length;
+// Пример кода с rest-оператором (...)
+{
+    function sum(...args) {
+        let s = 0;
+        let len = args.length;
 
-  for (let i = 0; i < len; i++) {
-    s += a[i];
-  }
+        for (let i = 0; i < len; i++) {
+            s += args[i];
+        }
 
-  return s;
+        return s;
+    }
+
+//                  0  1  2  3
+    console.log(sum(10, 5, 9, 2)); //26
 }
-//               0  1  2  3
-console.log(sum(10, 5, 9, 2)); //26
-
-
 
 // Создайте функцию avg(), которая найдет среднее арифметическое по всем своим аргументам
 // Например,
 // avg(3, 7, 20); //10
 // avg(100, 100, 400); //200
+{
+    function avg(...args) {
+//return sum(...args) / args.length; // (быстрый способ = сумма аргументов/кол-во=среднее)
+        const len = args.length;
+        let summa = 0;
+        for (let i = 0; i < len; i++) {
+            summa += args[i];
+        }
+        return summa / len;
+    }
 
-
+    console.log(avg(9, 9, 6, 6)); //7,5
+}
 
 // Создайте функцию graph(), которая построит в консоли график из звездочек
 // Например,
@@ -59,3 +71,16 @@ console.log(sum(10, 5, 9, 2)); //26
 // **********
 // *****
 // *
+{
+    function graph(...args) {
+        const len = args.length;
+        let x = '';
+        for (let i = 0; i < len; i++) {
+            x += '*'.repeat(args[i]) + '\n'; // '*'.repeat(args[i]) -функция, которая значит повторить '*' столько раз
+            // "args[i]"
+        }
+        return x;
+    }
+
+    console.log('%c' + graph(4, 7, 10, 5, 1), 'color:violet'); //в вывод добавлено CSS-свойство с цветом - методом '%c' + (то,что треб вывести), 'color:violet'
+}
